8dc22c43bce226b60868be6c5cb0cb7593f8d4df
Abdulaziz Kholikulov <phoenixaax2011@gmail.com>
2024-08-08 13:59:36 +0500

    final

---
+++ AUTH/migrations/000002_insert_tables.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, email, hashed_password, role, is_active, created_at)
+VALUES ('550e8400-e29b-41d4-a716-446655440000', 'superadmin@example.com', '$2a$10$Wz1S0Kt/OGxF9fhVtOXF3u.4O7X6FPF3Y9C.UFkqOeO4g5tEkx0fS', 'superadmin', TRUE, NOW());
