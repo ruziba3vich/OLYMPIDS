b18b712ad0551ea93225b3e216708781e858bd49
prodonik <rruzibayevich@gmail.com>
2024-08-08 11:52:00 +0500

    hhh

--- GATEWAY/internal/items/http/handler/event/event.handler.go
+++ GATEWAY/internal/items/http/handler/event/event.handler.go
@@ -4,23 +4,26 @@ import (
 	"log/slog"
 
 	pb "github.com/ruziba3vich/OLYMPIDS/GATEWAY/genproto/event"
+	"github.com/ruziba3vich/OLYMPIDS/GATEWAY/internal/items/msgbroker/event"
 	"github.com/ruziba3vich/OLYMPIDS/GATEWAY/internal/items/redisservice"
 )
 
 type (
 	EventHandler struct {
-		event  pb.EventServiceClient
-		logger *slog.Logger
-		redis  *redisservice.RedisService
+		event    pb.EventServiceClient
+		logger   *slog.Logger
+		redis    *redisservice.RedisService
+		msbroker *event.EventMsgBroker
 	}
 )
 
-func NewAthleteHandler(logger *slog.Logger, event pb.EventServiceClient, redis *redisservice.RedisService) *EventHandler {
+func NewEventHandler(logger *slog.Logger, event pb.EventServiceClient, redis *redisservice.RedisService, msbroker *event.EventMsgBroker) *EventHandler {
 	return &EventHandler{
 		event:  event,
 		logger: logger,
 		redis:  redis,
+		msbroker: msbroker,
 	}
 }
 
-
+// func (e *EventH/andler) Publish()
